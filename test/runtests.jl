0175f86bd5060eefaa66a2ce34e9c88b3bfa0fa8
riw <riw@wip.tu-berlin.de>
2022-03-23 17:42:51 +0100

    Add test for storage model.

--- test/runtests.jl
+++ test/runtests.jl
@@ -93,3 +93,36 @@ end
         MarketModel.add_chance_constraints!(pomato);
     end
 end
+
+@testset "StorageModel" begin
+    MarketModel.set_global_optimizer(Clp)	
+    data_dir = cd(pwd, "..")*"/examples/nrel_118/"
+    data = MarketModel.read_model_data(data_dir);
+    data.options["plant_types"]["es"] = ["storage"]
+    data.options["timeseries"]["market_horizon"] = 6
+
+    es = MarketModel.Plant(length(data.plants) + 1, "Storage", 1, 5., 0., 0.75, 1., 50., 0., "storage")
+    es.storage_capacity = 500
+    es.d_max = 50
+    es.inflow = zeros(24)
+    push!(data.plants, es)
+
+    @test !(isdefined(data.plants[end], :storage_level_start))
+
+    MarketModel.set_storage_levels!(data)
+
+    @test isdefined(data.plants[end], :storage_level_start)
+    @test isdefined(data.plants[end], :storage_level_end)
+    
+    data_full = deepcopy(data)
+    model_horizon_segments = MarketModel.split_timeseries_segments(
+        data_full, data.options["timeseries"]["market_horizon"]
+    )
+    @test length(data.plants[end].storage_level_end) == length(model_horizon_segments)
+    for (i, timesteps) in enumerate(model_horizon_segments)
+        data = deepcopy(data_full)
+        data.t = data.t[timesteps]
+        MarketModel.set_model_horizon!(data, i)
+        @test length(data.t) == 6
+    end
+end
